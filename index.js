dc631d6c82acf2c9f0cd6661f4a0da1170732242
Yury V <kotden09@gmail.com>
2025-02-28 21:03:48 +0300

    faet: develop left menu

---
+++ index.js
@@ -0,0 +1,15 @@
+(() => {
+    "use strict";
+    
+    
+    function menuInit() {
+        if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
+            if (e.target.closest(".icon-menu")) {
+                document.documentElement.classList.toggle("menu-open");
+            }
+        }));
+    }
+    
+    menuInit();
+    
+})();
